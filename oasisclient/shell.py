c1e9a0c9dc3fc9f6afdd71087db5e44c07592c9a
kjwook <cmdhema@gmail.com>
2016-08-30 15:42:03 +0900

    update shell.py

--- oasisclient/shell.py
+++ oasisclient/shell.py
@@ -20,6 +20,27 @@ DEFAULT_SERVICE_TYPE = 'container-infra'
 
 logger = logging.getLogger(__name__)
 
+class OasisClientArgumentParser(argparse.ArgumentParser):
+
+    def __init__(self, *args, **kwargs):
+        super(OasisClientArgumentParser, self).__init__(*args, **kwargs)
+
+    def error(self, message):
+        """error(message: string)
+
+        Prints a usage message incorporating the message to stderr and
+        exits.
+        """
+        self.print_usage(sys.stderr)
+        # FIXME(lzyeval): if changes occur in argparse.ArgParser._check_value
+        choose_from = ' (choose from'
+        progparts = self.prog.partition(' ')
+        self.exit(2, "error: %(errmsg)s\nTry '%(mainp)s help %(subp)s'"
+                     " for more information.\n" %
+                     {'errmsg': message.split(choose_from)[0],
+                      'mainp': progparts[0],
+                      'subp': progparts[2]})
+
 class HelpFormatter(argparse.HelpFormatter):
     def start_section(self, heading):
         # Title-case the headings
@@ -34,46 +55,14 @@ class OasisShell(object):
         logging.getLogger('iso8601').setLevel(logging.WARNING)
         logging.getLogger('urllib3.connectionpool').setLevel(logging.WARNING)
 
-    def _add_bash_completion_subparser(self, subparsers):
-        subparser = (
-            subparsers.add_parser('bash_completion',
-                                  add_help=False,
-                                  formatter_class=OpenStackHelpFormatter)
-        )
-        self.subcommands['bash_completion'] = subparser
-        subparser.set_defaults(func=self.do_bash_completion)
-
-    def _find_actions(self, subparsers, actions_module):
-        for attr in (a for a in dir(actions_module) if a.startswith('do_')):
-            # I prefer to be hyphen-separated instead of underscores.
-            command = attr[3:].replace('_', '-')
-            callback = getattr(actions_module, attr)
-            desc = callback.__doc__ or ''
-            action_help = desc.strip()
-            arguments = getattr(callback, 'arguments', [])
-
-            subparser = (
-                subparsers.add_parser(command,
-                                      help=action_help,
-                                      description=desc,
-                                      add_help=False,
-                                      formatter_class=OpenStackHelpFormatter)
-            )
-            subparser.add_argument('-h', '--help',
-                                   action='help',
-                                   help=argparse.SUPPRESS,)
-            self.subcommands[command] = subparser
-            for (args, kwargs) in arguments:
-                subparser.add_argument(*args, **kwargs)
-            subparser.set_defaults(func=callback)
-
     def get_base_parser(self):
-        parser = argparse.ArgumentParser(
+        parser = OasisClientArgumentParser(
             prog='oasis',
-            description=-None,
-            epilog='Type "selin help <COMMAND>" for help on a specific' 'command.',
+            description=None,
+            epilog='See "oasis help COMMAND" '
+                   'for help on a specific command.',
             add_help=False,
-            formatter_class=HelpFormatter
+            formatter_class=OpenStackHelpFormatter,
         )
         # Global arguments
         parser.add_argument('-h', '--help',
@@ -83,6 +72,9 @@ class OasisShell(object):
         parser.add_argument('--version',
                             action='version',
                             version=version.version_info.version_string())
+
+        return parser
+
     def get_subcommand_parser(self, version):
         parser = self.get_base_parser()
 
@@ -104,6 +96,18 @@ class OasisShell(object):
 
         return parser
 
+    def setup_debugging(self, debug):
+        if debug:
+            streamformat = "%(levelname)s (%(module)s:%(lineno)d) %(message)s"
+            # Set up the root logger to debug so that the submodules can
+            # print debug messages
+            logging.basicConfig(level=logging.DEBUG,
+                                format=streamformat)
+        else:
+            streamformat = "%(levelname)s %(message)s"
+            logging.basicConfig(level=logging.CRITICAL,
+                                format=streamformat)
+
 
     def main(self, argv):
 
@@ -114,7 +118,7 @@ class OasisShell(object):
         # Parse args once to find version and debug settings
         parser = self.get_base_parser()
         (options, args) = parser.parse_known_args(argv)
-        self.setup_debugging(options.debug)
+        # self.setup_debugging(options.debug)
 
         # NOTE(dtroyer): Hackery to handle --endpoint_type due to argparse
         #                thinking usage-list --end is ambiguous; but it
@@ -124,63 +128,63 @@ class OasisShell(object):
             spot = argv.index('--endpoint_type')
             argv[spot] = '--endpoint-type'
 
-        subcommand_parser = (
-            self.get_subcommand_parser(options.magnum_api_version)
-        )
-        self.parser = subcommand_parser
-
-        if options.help or not argv:
-            subcommand_parser.print_help()
-            return 0
-
-        args = subcommand_parser.parse_args(argv)
-
-        # Short-circuit and deal with help right away.
-        # NOTE(jamespage): args.func is not guaranteed with python >= 3.4
-        if not hasattr(args, 'func') or args.func == self.do_help:
-            self.do_help(args)
-            return 0
-        elif args.func == self.do_bash_completion:
-            self.do_bash_completion(args)
-            return 0
-
-        if not args.service_type:
-            args.service_type = DEFAULT_SERVICE_TYPE
-
-        if args.bypass_url:
-            args.os_endpoint_override = args.bypass_url
-
-        args.os_project_id = (args.os_project_id or args.os_tenant_id)
-        args.os_project_name = (args.os_project_name or args.os_tenant_name)
-
-        if not utils.isunauthenticated(args.func):
-            if (not (args.os_token and
-                     (args.os_auth_url or args.os_endpoint_override)) and
-                not args.os_cloud
-                ):
-
-                if not (args.os_username or args.os_user_id):
-                    raise exc.CommandError(
-                        "You must provide a username via either --os-username "
-                        "or via env[OS_USERNAME]"
-                    )
-                if not args.os_password:
-                    raise exc.CommandError(
-                        "You must provide a password via either "
-                        "--os-password, env[OS_PASSWORD], or prompted "
-                        "response"
-                    )
-                if (not args.os_project_name and not args.os_project_id):
-                    raise exc.CommandError(
-                        "You must provide a project name or project id via "
-                        "--os-project-name, --os-project-id, "
-                        "env[OS_PROJECT_NAME] or env[OS_PROJECT_ID]"
-                    )
-                if not args.os_auth_url:
-                    raise exc.CommandError(
-                        "You must provide an auth url via either "
-                        "--os-auth-url or via env[OS_AUTH_URL]"
-                    )
+        # subcommand_parser = (
+        #     self.get_subcommand_parser(options.magnum_api_version)
+        # )
+        # self.parser = subcommand_parser
+        #
+        # if options.help or not argv:
+        #     subcommand_parser.print_help()
+        #     return 0
+        #
+        # args = subcommand_parser.parse_args(argv)
+        #
+        # # Short-circuit and deal with help right away.
+        # # NOTE(jamespage): args.func is not guaranteed with python >= 3.4
+        # if not hasattr(args, 'func') or args.func == self.do_help:
+        #     self.do_help(args)
+        #     return 0
+        # elif args.func == self.do_bash_completion:
+        #     self.do_bash_completion(args)
+        #     return 0
+        #
+        # if not args.service_type:
+        #     args.service_type = DEFAULT_SERVICE_TYPE
+        #
+        # if args.bypass_url:
+        #     args.os_endpoint_override = args.bypass_url
+        #
+        # args.os_project_id = (args.os_project_id or args.os_tenant_id)
+        # args.os_project_name = (args.os_project_name or args.os_tenant_name)
+        #
+        # if not utils.isunauthenticated(args.func):
+        #     if (not (args.os_token and
+        #              (args.os_auth_url or args.os_endpoint_override)) and
+        #         not args.os_cloud
+        #         ):
+        #
+        #         if not (args.os_username or args.os_user_id):
+        #             raise exc.CommandError(
+        #                 "You must provide a username via either --os-username "
+        #                 "or via env[OS_USERNAME]"
+        #             )
+        #         if not args.os_password:
+        #             raise exc.CommandError(
+        #                 "You must provide a password via either "
+        #                 "--os-password, env[OS_PASSWORD], or prompted "
+        #                 "response"
+        #             )
+        #         if (not args.os_project_name and not args.os_project_id):
+        #             raise exc.CommandError(
+        #                 "You must provide a project name or project id via "
+        #                 "--os-project-name, --os-project-id, "
+        #                 "env[OS_PROJECT_NAME] or env[OS_PROJECT_ID]"
+        #             )
+        #         if not args.os_auth_url:
+        #             raise exc.CommandError(
+        #                 "You must provide an auth url via either "
+        #                 "--os-auth-url or via env[OS_AUTH_URL]"
+        #             )
         try:
             client = {
                 '1': client_v1,
@@ -188,6 +192,27 @@ class OasisShell(object):
         except KeyError:
             client = client_v1
 
+        self.cs = client.Client(
+            cloud=args.os_cloud,
+            user_id=args.os_user_id,
+            username=args.os_username,
+            password=args.os_password,
+            auth_token=args.os_token,
+            project_id=args.os_project_id,
+            project_name=args.os_project_name,
+            user_domain_id=args.os_user_domain_id,
+            user_domain_name=args.os_user_domain_name,
+            project_domain_id=args.os_project_domain_id,
+            project_domain_name=args.os_project_domain_name,
+            auth_url=args.os_auth_url,
+            service_type=args.service_type,
+            region_name=args.os_region_name,
+            magnum_url=args.os_endpoint_override,
+            interface=args.os_interface,
+            insecure=args.insecure,
+        )
+
+        args.func(self.cs, args)
 # I'm picky about my shell help.
 class OpenStackHelpFormatter(argparse.HelpFormatter):
     def start_section(self, heading):
@@ -195,14 +220,15 @@ class OpenStackHelpFormatter(argparse.HelpFormatter):
         heading = '%s%s' % (heading[0].upper(), heading[1:])
         super(OpenStackHelpFormatter, self).start_section(heading)
 
+
 def main():
-    try:
+#    try:
         OasisShell().main(map(encodeutils.safe_decode, sys.argv[1:]))
 
-    except Exception as e:
-        logger.debug(e, exc_info=1)
-        print("ERROR: %s" % encodeutils.safe_encode(six.text_type(e)))
-        sys.exit(1)
+#    except Exception as e:
+#        logger.debug(e, exc_info=1)
+#        print("ERROR: %s" % encodeutils.safe_encode(six.text_type(e)))
+#        sys.exit(1)
 
 if __name__ == "__main__":
     main()
