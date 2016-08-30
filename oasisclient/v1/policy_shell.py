303a5f52389548397ae1048561849a33cdfbedda
kjwook <cmdhema@gmail.com>
2016-08-30 16:59:38 +0900

    update policy shell

---
+++ oasisclient/v1/policy_shell.py
@@ -0,0 +1,24 @@
+from oasisclient.common import utils
+
+
+@utils.arg('--policy',
+           metavar='<policy>',
+           help='VM policy to create.')
+def do_function_delete(cs, args):
+    """Create a function."""
+    pass
+
+
+
+def do_function_list(cs, args):
+    """Print a list of policy."""
+    pass
+
+@utils.arg('policy',
+           metavar='<policy>',
+           nargs='+',
+           help='ID or name of the (policy)s to delete.')
+def do_function_delete(cs, args):
+    """Delete specified policy."""
+    pass
+
